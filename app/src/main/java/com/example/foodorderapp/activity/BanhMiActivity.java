d9295d32c1a20d23249f8c9b07c39852bbb3d901
tuan020501 <92625430+tuan020501@users.noreply.github.com>
2021-11-09 14:44:31 +0700

    listview

---
+++ app/src/main/java/com/example/foodorderapp/activity/BanhMiActivity.java
@@ -0,0 +1,39 @@
+package com.example.foodorderapp.activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.foodorderapp.R;
+import com.example.foodorderapp.activity.ListviewMon.Mon;
+import com.example.foodorderapp.activity.ListviewMon.MonAdapter;
+
+import java.util.ArrayList;
+
+public class BanhMiActivity extends AppCompatActivity {
+    ListView lvmonts;
+    ArrayList<Mon> arraymon;
+    MonAdapter monAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.banhmi);
+        Button buton11= (Button) findViewById(R.id.btnGioHang);
+        Anhxa();
+        monAdapter = new MonAdapter(this,R.layout.dongmon,arraymon);
+        lvmonts.setAdapter(monAdapter);
+
+
+
+    }
+    private void Anhxa (){
+        lvmonts =(ListView) findViewById(R.id.LvTraSua);
+        arraymon = new ArrayList<>();
+        arraymon.add(new Mon("Bánh Mì Thịt","25000", R.drawable.banhmithit));
+        arraymon.add(new Mon("Bánh Mì Bơ Tỏi","25000", R.drawable.banhmibotoi));
+
+    }
+}
