8db52cc45879cd8dffcdc834681162e168ec45f8
nguyenthicamtu-2307 <84969501+nguyenthicamtu-2307@users.noreply.github.com>
2021-12-11 16:30:57 +0700

    API login

--- app/src/main/java/com/example/foodorderapp/service/APIService.java
+++ app/src/main/java/com/example/foodorderapp/service/APIService.java
@@ -1,23 +1,34 @@
 package com.example.foodorderapp.service;
 
 import com.example.foodorderapp.activity.ListviewMon.Mon;
+import com.example.foodorderapp.model.KhachHang;
+import com.example.foodorderapp.model.Loginrequired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface APIService {
-    @GET("listar")
+    @GET("sanpham/listar")
     Call<List<Mon>> getsanpham();
-    @GET("listts")
+    @GET("sanpham/listts")
     Call<List<Mon>> gettrasua();
-    @GET("listcf")
+    @GET("sanpham/listcf")
     Call<List<Mon>> getcoffe();
-    @GET("listbm")
+    @GET("sanpham/listbm")
     Call<List<Mon>> getbread();
-    @GET("listbsn")
+    @GET("sanpham/listbsn")
     Call<List<Mon>> getcake();
-    @GET("dexuat")
+    @GET("sanpham/dexuat")
     Call<List<Mon>> dexuat();
+    @GET("khachhang/listar")
+    Call<List<KhachHang>> khachhang();
+
+
 }
